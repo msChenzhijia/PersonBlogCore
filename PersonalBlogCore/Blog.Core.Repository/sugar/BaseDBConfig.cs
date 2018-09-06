67ec4cf67da166b28c8ac66c916e6c974b7db368
Chen <>
2018-09-06 17:38:35 +0800

    修改了BaseDBConfig

--- PersonalBlogCore/Blog.Core.Repository/sugar/BaseDBConfig.cs
+++ PersonalBlogCore/Blog.Core.Repository/sugar/BaseDBConfig.cs
@@ -10,7 +10,7 @@ namespace Blog.Core.Repository.sugar
 
         //正常格式是
 
-        public static string ConnectionString = "server=.;uid=sa;pwd=sa;database=BlogDB"; 
+        public static string ConnectionString = "server=localhost;user=root;password=123;post=3306;database=personalblogcore;sslmode=none;";
 
         //原谅我用配置文件的形式，因为我直接调用的是我的服务器账号和密码，安全起见
     }
