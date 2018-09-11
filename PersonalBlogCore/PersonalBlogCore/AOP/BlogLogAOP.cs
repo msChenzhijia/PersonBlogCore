459babfb4df309ad9f3214823bb13e1c61dc28c9
Chen <>
2018-09-11 17:40:16 +0800

    新增日志文件

--- PersonalBlogCore/PersonalBlogCore/AOP/BlogLogAOP.cs
+++ PersonalBlogCore/PersonalBlogCore/AOP/BlogLogAOP.cs
@@ -1,6 +1,7 @@
 ﻿using Castle.DynamicProxy;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,7 +19,18 @@ namespace PersonalBlogCore.AOP
         public void Intercept(IInvocation invocation)
         {
             //记录被拦截方法信息的日志信息
-
+            var dataIntercept = $"{DateTime.Now.ToString("yyyyMMddHHmmss")}" + $"当前执行方法:{invocation.Method.Name}" + $"参数是{string.Join(", ", invocation.Arguments.Select(a => (a ?? "").ToString())).ToArray()}";
+            //在被拦截的方法执行完毕后,继续执行当前方法
+            invocation.Proceed();
+            dataIntercept += ($"被拦截方法执行完毕,返回结果:{invocation.ReturnValue}");
+            #region 输出到当前项目日志
+            var path = Directory.GetCurrentDirectory() + @"\Log";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filename = path + $@"\InterceptLog-{DateTime.Now.ToString("yyyyMMddHHmmss")}.log";
+            #endregion
         }
     }
 }
